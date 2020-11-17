1ecd4177ec9e654479efbb92aae03cd187cfa0be
Elizabeth McHughes <emchughes@uco.edu>
2020-11-17 13:04:33 -0600

    added team info class

---
+++ lib/models/teamInfo.dart
@@ -0,0 +1,45 @@
+class TeamInfo {
+  //field name for Firestore documents
+  static const COLLECTION = 'teamInfo';
+  static const TEAM_NAME = 'teamName';
+  static const SPORT = 'sport';
+  static const TEAM_MEMBERS = 'teamMembers';
+
+  String docId;
+  String teamName;
+  String sport;
+  List<dynamic> teamMembers; // list of all team members
+
+  TeamInfo({
+    this.docId,
+    this.teamName,
+    this.sport,
+    this.teamMembers,
+  }) {
+    this.teamMembers ??= [];
+  }
+
+  // convert Dart object to Firestore document
+  Map<String, dynamic> serialize() {
+    return <String, dynamic>{
+      TEAM_NAME: teamName,
+      SPORT: sport,
+      TEAM_MEMBERS: teamMembers,
+    };
+  }
+
+  // convert Firestore doc to Dart object
+  static TeamInfo deserialize(Map<String, dynamic> data, String docId) {
+    return TeamInfo(
+      docId: docId,
+      teamName: data[TeamInfo.TEAM_NAME],
+      sport: data[TeamInfo.SPORT],
+      teamMembers: data[TeamInfo.TEAM_MEMBERS],
+    );
+  }
+
+  @override
+  String toString() {
+    return '$docId $teamName $sport $teamMembers';
+  }
+}
